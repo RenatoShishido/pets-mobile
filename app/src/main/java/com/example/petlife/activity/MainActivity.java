fca38355e5e5ee3f661f7c979ed009ddb8e40d1d
higor-henrique <you@example.com>
2021-06-12 01:46:09 -0400

    add menu drawer

--- app/src/main/java/com/example/petlife/activity/MainActivity.java
+++ app/src/main/java/com/example/petlife/activity/MainActivity.java
@@ -1,21 +1,35 @@
-package com.example.petlife;
-
+package com.example.petlife.activity;
 
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.Button;
 
+
+import com.example.petlife.R;
 import com.example.petlife.fragment.*;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-    Button btnAdotar,btnDoar,btnFavoritos;
+    Button btnAdotar, btnDoar, btnFavoritos;
     boolean isLogged = false;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +52,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDoar = findViewById(R.id.btnDoar);
         btnFavoritos = findViewById(R.id.btnFavoritos);
         CadastroDoacaoFragment doarFragment = new CadastroDoacaoFragment();//add fragment to transaction
-        fragmentTransaction.replace(R.id.fragment_container,doarFragment);
+        fragmentTransaction.replace(R.id.fragment_container, doarFragment);
         fragmentTransaction.commit();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
 
     }
 
@@ -48,47 +75,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        if(view.equals(btnDoar)){
-
-            CadastroDoacaoFragment doarFragment = new CadastroDoacaoFragment();//add fragment to transaction
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.replace(R.id.fragment_container,doarFragment);
-            fragmentTransaction.commit();
+        if (view.equals(btnDoar)) {
+            if (isLogged) {
 
 
+                CadastroDoacaoFragment doarFragment = new CadastroDoacaoFragment();//add fragment to transaction
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.replace(R.id.fragment_container, doarFragment);
+                fragmentTransaction.commit();
+            } else {
+                HandleLoginFragment handleLoginFragment = new HandleLoginFragment();
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.replace(R.id.fragment_container, handleLoginFragment);
+                fragmentTransaction.commit();
 
+            }
 
-        }else if(view.equals(btnFavoritos)){
-            if(isLogged) {
+        } else if (view.equals(btnFavoritos)) {
+            if (isLogged) {
 
                 ListagemFavoritoFragment favoritoFragment = new ListagemFavoritoFragment();
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.replace(R.id.fragment_container, favoritoFragment);
                 fragmentTransaction.commit();
-            }
-            else {
+            } else {
                 HandleLoginFragment handleLoginFragment = new HandleLoginFragment();
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.replace(R.id.fragment_container, handleLoginFragment);
                 fragmentTransaction.commit();
             }
-        }else if(view.equals(btnAdotar)){
+        } else if (view.equals(btnAdotar)) {
 
-            if(isLogged) {
-                ListagemPetFragment adotarFragment = new ListagemPetFragment();
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.replace(R.id.fragment_container, adotarFragment);
-                fragmentTransaction.commit();
-            }
-            else {
-                HandleLoginFragment handleLoginFragment = new HandleLoginFragment();
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.replace(R.id.fragment_container, handleLoginFragment);
-                fragmentTransaction.commit();
 
-            }
+            ListagemPetFragment adotarFragment = new ListagemPetFragment();
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.replace(R.id.fragment_container, adotarFragment);
+            fragmentTransaction.commit();
+
         }
+
+
     }
 
 
+    @Override
+    public void onBackPressed() {
+
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: break; case R.id.nav_bus:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+//            case R.id.nav_login: menu.findItem(R.id.nav_logout).setVisible(true);
+//                menu.findItem(R.id.nav_profile).setVisible(true);
+//                menu.findItem(R.id.nav_login).setVisible(false);
+//                break;
+//            case R.id.nav_logout: menu.findItem(R.id.nav_logout).setVisible(false);
+//                menu.findItem(R.id.nav_profile).setVisible(false);
+//                menu.findItem(R.id.nav_login).setVisible(true);
+//                break;
+//            case R.id.nav_share: Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show(); break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START); return true;
+    }
 }
--- app/src/main/java/com/example/petlife/fragment/HandleLoginFragment.java
+++ app/src/main/java/com/example/petlife/fragment/HandleLoginFragment.java
@@ -1,66 +1,39 @@
 package com.example.petlife.fragment;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
-import androidx.fragment.app.Fragment;
-
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.petlife.activity.LoginActivity;
+import com.example.petlife.activity.MainActivity;
 import com.example.petlife.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HandleLoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HandleLoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HandleLoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HandleLoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HandleLoginFragment newInstance(String param1, String param2) {
-        HandleLoginFragment fragment = new HandleLoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+   Button btnFazerLogin;
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater,  @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_handle_login,container,false);
+        btnFazerLogin = view.findViewById(R.id.btnFazerLogin);
+
+        btnFazerLogin.setOnClickListener(v -> {
+                Intent i = new Intent(getActivity(),LoginActivity.class);
+                startActivity(i);
+        });
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_handle_login, container, false);
-    }
+
 }
